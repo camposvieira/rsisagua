ba412d0492a791129ab4efa19b24bb20c6cf4d14
Gabriel Campos Vieira <camposvieiragabriel@gmail.com>
2022-06-26 18:52:43 -0300

    object documentation ciano

--- R/download_ciano.R
+++ R/download_ciano.R
@@ -1,6 +1,23 @@
-
-
-#Function download_ciano (Cianotoxinas e cianobactérias)
+#'Function download_ciano (Cianotoxinas e cianobactérias)
+#'
+#'@param periodo Selecao do ou dos anos de referencia, a partir de 2014.
+#'@param regiao Selecao da ou das regioes geograficas.
+#'@param unidade_federativa Selecao da ou das unidades federativas.
+#'@return Data frame do conjunto de dados com filtros a partir dos parametros da funcao
+#'@export
+#'@examples
+#'df <- download_ciano(c(2014:2020), "NORTE", c("AM", "PA", "RO"))
+#'df2 <- download_ciano(2020, unidade_federativa = "SC")
+#'df3 <- download_ciano(2015, regiao = "NORTE")
+#'df4 <- download_ciano(2022)
+#'\dontrun{
+#'df5 <- download_ciano(regiao = "NORTE")
+#'df6 <- download_ciano(unidade_federativa = "CE")
+#'}
+#'
+#'@note O periodo de dados e um campo obrigatorio e inicia em 2014 ate o ano atual.
+#'Os parametros regiao e unidade_federativa devem sempre ser inseridos com letra
+#'maiuscula e entre aspas, conforme exemplo.
 
 download_ciano <- function(periodo,regiao=NULL,unidade_federativa=NULL){
 
@@ -29,7 +46,7 @@ download_ciano <- function(periodo,regiao=NULL,unidade_federativa=NULL){
   #Load data and filter
   ciano <- data.table::fread(datazip, sep=";") %>%
     janitor::clean_names() %>%
-    dplyr::filter(ano_de_referencia %in% periodo) %>%
+    dplyr::filter(ano %in% periodo) %>%
     dplyr::filter(regiao_geografica %in% regiao) %>%
     dplyr::filter(uf %in% unidade_federativa)
 
