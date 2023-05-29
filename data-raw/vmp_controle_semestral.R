e9a06ee9c54f50b717d468f7dcb4939d8fe31b25
Gabriel Campos Vieira <camposvieiragabriel@gmail.com>
2023-05-29 16:55:19 -0300

    update options scipen in data raw

--- data-raw/vmp_controle_semestral.R
+++ data-raw/vmp_controle_semestral.R
@@ -1,6 +1,8 @@
 #Carregando a base dos VMPs de controle semestral
 
-lista_vmp_controle_semestral <- read.csv2("vmp_cs.csv", row.names = NULL)
+lista_vmp_controle_semestral <- read.csv2("vmp_cs.csv", row.names = NULL , options(scipen = 999) )
 
-#Exportanto para a pasta /data
+options(scipen = 999)
+
+#Exportando para a pasta /data
 usethis::use_data(lista_vmp_controle_semestral, overwrite = T)
